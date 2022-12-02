82cd0385c3e88ffad06f40672cb1a342a3b6afb5
Andrei Balasanov <andrei.balasanov@gmail.com>
2022-12-02 11:16:27 +0200

    fdserver streaming intergation

--- fdserver.py
+++ fdserver.py
@@ -11,37 +11,50 @@ from datetime import datetime
 import threading
 import numpy as np
 import sys
-
+import io 
 from flask_cors import CORS
 from PIL import Image
 from engine.tfengine import Engine as TFEngine
 from utils import imgannotate
 from logic import detector_filter
-
+import argparse
 
 import logger
 from logger import getLogger
 logger = getLogger(__name__)
 
 
+from io import StringIO
+import cv2
 
 from pathlib import Path
 import sys
+import requests
 
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     bundle_dir = Path(sys._MEIPASS)
 else:
     bundle_dir = Path(__file__).parent
 
-
 path_to_dat = Path.cwd() / bundle_dir 
 print ("Path to dat is:",path_to_dat)
 
+streaming_url = ""
 app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 start = time.time()
+
+
+def get_image(url):
+	response = requests.get(url)
+	if response.status_code == 200:
+		with io.BytesIO(response.content) as f:
+			with Image.open(f) as img:
+				res = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+				return res
+	return None
 
 engine = TFEngine(path_to_dat)
 
@@ -62,11 +75,7 @@ def locationsToJson (locations):
 @app.route('/api/v1/detect', methods=['POST'])
 def detect():
 	try:    
-		if 'file' not in request.files:
-			raise Exception ("No file")
-		file = request.files['file']
-		img = Image.open(request.files['file'].stream)
-		cvimg = np.array(img)
+		cvimg = get_image_from_request (request)
 		locations,desc =  engine.process (cvimg)
 		info = {}
 		locations = detector_filter (locations)
@@ -90,11 +99,7 @@ def detect():
 @app.route('/api/v1/detect_faces', methods=['POST'])
 def detect_faces():
 	try:
-		if 'file' not in request.files:
-			raise Exception ("No file")
-		file = request.files['file']
-		img = Image.open(request.files['file'].stream)
-		cvimg = np.array(img) 
+		cvimg = get_image_from_request (request)
 		locations,desc =  engine.process (cvimg)
 		ret = {}
 		locations = detector_filter (locations)
@@ -111,14 +116,20 @@ def serve_pil_image(pil_img):
 	img_io.seek(0)
 	return send_file(img_io, mimetype='image/jpeg')
 
+def get_image_from_request (req):
+	cvimg = None
+	if 'file' not in req.files:
+		cvimg = get_image (streaming_url)
+	else:
+		file = req.files['file']
+		img = Image.open(request.files['file'].stream)
+		cvimg = np.array(img)
+	return cvimg
+
 @app.route('/api/v1/annotate', methods=['POST'])
 def annotate():
 	try:
-		if 'file' not in request.files:
-			raise Exception ("No file")
-		file = request.files['file']
-		img = Image.open(request.files['file'].stream)
-		cvimg = np.array(img)
+		cvimg = get_image_from_request (request)
 		locations,desc =  engine.process (cvimg)
 		ret = imgannotate (detector_filter(locations),cvimg)
 		return serve_pil_image (ret)
@@ -145,5 +156,39 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 	})
 app.register_blueprint(swaggerui_blueprint)
 
+def my_url(arg):
+	from urllib.parse import urlparse
+	url = urlparse(arg)
+	if all((url.scheme, url.netloc)):  # possibly other sections?
+		return arg  # return url in case you need the parsed object
+	raise ArgumentTypeError('Invalid URL')
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080, debug=False)
+
+
+	parser = argparse.ArgumentParser(description='Face detection server.')
+
+        
+	parser.add_argument(
+		"-p",   
+		"--port",
+		type=int,
+		default=8080,
+		help="Server port",
+	)
+
+
+	parser.add_argument(
+		"-u",
+    		"--url",
+    		type=my_url,
+		default="http://192.168.38.11:1000/image",
+		help="Image streaming server",
+	)
+
+	
+	args = parser.parse_args()
+	port  = vars(args)["port"]
+	streaming_url = vars(args)["url"]
+
+	app.run(host="0.0.0.0",port=port, debug=False)
